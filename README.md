eb46d9fa44fa1bb8eccd9dba14d6b680d8b448ea
Mitchell <hocke079@umn.edu>
2020-03-22 15:02:56 -0500

    Now runs a nodeJS httpServer

--- README.md
+++ README.md
@@ -4,16 +4,9 @@ To run:
 
 Download the code
 
-Switch to directory
-
-Run python command:
-
-python -m http.server 80
-
-go to localhost
-
-to access the nodeJs stuff (server)
-
 Install nodeJs
 
-node ./server/serverMain.js
+npm install -g websocket
+npm install node-static
+
+node ./server/httpServer.js
--- server/serverMain.js
+++ server/serverMain.js
@@ -4,26 +4,13 @@ const fs = require('fs');
 const WebSocketServer = require('websocket').server;
 
 //Create a http server
-const server = http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-})
-server.listen(8000);
+const server = http.createServer()
+server.listen(80);
 
 //wrap the http server with the web socket server
 const wsServer = new WebSocketServer({
     httpServer: server
 });
-
-//Probably should serve files somewhere in here
-
 
 //I think request is a keyword
 wsServer.on('request', function(request) {
